e04c7d7f8ebce8a23a4191be2f4fbe83f1d7cbe0
deepakchauhan2096 <deepakchauhan2096@gmail.com>
2023-08-05 14:17:55 -0700

    ff

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import Apps from './Apps';
+import App from './App';
 
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <Apps />
+    <App />
   </Provider>,
   document.getElementById('root')
 );
